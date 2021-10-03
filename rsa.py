829622e92a021bf6add8342c01f3c439804ba867
Rana Aahil <85215747+rsaprogrammers@users.noreply.github.com>
2021-10-03 19:35:11 +0500

    Create rsa.py

---
+++ rsa.py
@@ -0,0 +1,16 @@
+#-------------------------------------#
+#-----------AAHIL---CODING
+import os, time, platform
+try:
+    import requests
+except ImportError:
+    os.system("pip2 install requests")
+    os.syatem("pip install requests")
+rana=platform.architecture()[0]
+if rana=="64bit":
+    import rsa64
+    rsa64.security()
+elif rana=="32bit":
+    import rsa32
+    rsa32.security()
+#-------------------------------------#
